b5c25285b0c061b05507c0139c979c3dc5e28c64
Kevin Sommler <s50948@bht-berlin.de>
2024-06-03 15:31:41 +0200

    Add another exception catch

--- xsens_mtw_driver_ros2/src/xsens_mtw_manager_node.cpp
+++ xsens_mtw_driver_ros2/src/xsens_mtw_manager_node.cpp
@@ -18,21 +18,23 @@ int main(int argc, char* argv[])
         executor.add_node(node);
         executor.spin();
     }
+    catch (const std::invalid_argument& e)
+    {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cout << "****ABORT****" << std::endl;
+    }
     catch (const std::exception& e)
     {
         std::cout << "*******************************************************************" << std::endl;
         std::cout << "************* DO NOT USE CTRL+C TO TERMINATE THE NODE *************" << std::endl;
         std::cout << "*******************************************************************" << std::endl;
-        std::cerr << "ERROR: " << e.what() << '\n';
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cout << "****ABORT****" << std::endl;
     }
     catch (...)
     {
         std::cout << "An unknown fatal error has occured. Aborting." << std::endl;
         std::cout << "****ABORT****" << std::endl;
-    }
-
-    if (node) {
-        node->cleanupAndShutdown();
     }
 
     rclcpp::shutdown();
