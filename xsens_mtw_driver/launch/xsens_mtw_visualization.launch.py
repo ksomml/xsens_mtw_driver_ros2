02683a52946532c23add8a909d4d27aed83039e1
ksomml <sommler@live.de>
2025-07-23 18:53:08 +0200

    style: adjust docstrings and path to params file for visualization node

--- xsens_mtw_driver/launch/xsens_mtw_visualization.launch.py
+++ xsens_mtw_driver/launch/xsens_mtw_visualization.launch.py
@@ -7,16 +7,16 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
-    """Launch configuration for the xsens_mtw_driver's IMU visualization Tool."""
+    """Launch configuration for the MTw visualization node of the xsens_mtw_driver package."""
     mtw_driver_path = get_package_share_path('xsens_mtw_driver')
-    imu_mapping_config = Path(mtw_driver_path) / 'config/imu_mapping.yaml'
+    ros_config_params = Path(mtw_driver_path) / 'config/params.yaml'
 
     xsens_mtw_visualization_node = Node(
         package='xsens_mtw_driver',
         name='xsens_mtw_visualization',
         executable='xsens_mtw_visualization',
         output='screen',
-        parameters=[imu_mapping_config],
+        parameters=[ros_config_params],
     )
 
     return LaunchDescription(
